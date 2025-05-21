7d9551e56ed14cf8eb2882814726d6b43b2b0d5b
vjayaraman <vj@emlix.com>
2025-05-21 16:22:05 +0200

    refactor : include benchmark test in ci

---
+++ ci/run_benchmark.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+CMD_PATH="$(realpath "$(dirname "$0")")"
+BASE_DIR="$(realpath "$CMD_PATH/..")"
+BUILD_TYPE="${1:-Debug}"
+
+. "$BASE_DIR/ci/common_names.sh"
+
+BENCHMARK_RESULT_DIR=${BENCHMARK_RESULT_DIR:-"$RESULT_DIR/benchmark_results"}
+
+export BUILD_TYPE
+export BENCHMARK_RESULT_DIR
+export BUILD_DIR
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}$BASE_DIR/build/deps/lib:$DIST_DIR/usr/local/lib"
+
+$BASE_DIR/test/benchmark/bench_test.sh
+
+FAIL=0
+for CSV_FILE in $(find ${BENCHMARK_RESULT_DIR} -name "*.csv" -type f); do
+    echo $CSV_FILE
+    LINE_COUNT=$(cat $CSV_FILE | wc -l)
+    if [ ${LINE_COUNT} -lt 2 ]; then
+        echo "No results for ${CSV_FILE}"
+        let FAIL++;
+    else
+        echo "Generate plot for '$CSV_FILE' .."
+        gnuplot -e report_file=\'$CSV_FILE\' $CMD_PATH/plot_csv_report.p
+    fi
+done
+
+exit ${FAIL}

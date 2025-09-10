30663cc01b68c101b9f61ecc3201d3e65de0d227
Friedrich Schwedler <fschwedler@emlix.com>
2025-09-10 14:48:31 +0200

    utiest(fix): nodes of value type string must be heap allocated

--- test/utest/samconf/samconf/samconfConfigSetBool/case_success_set_bool.c
+++ test/utest/samconf/samconf/samconfConfigSetBool/case_success_set_bool.c
@@ -1,37 +1,38 @@
 // SPDX-License-Identifier: MIT
 #include <stdint.h>
+#include <string.h>
 
 #include "samconfConfigSetBool_utest.h"
 
-int samconfTestSamconfConfigSetBoolSuccessSetup(UNUSED void **state) {
+int samconfTestSamconfConfigSetBoolSuccessSetup(void **state) {
+    samconfConfig_t *root = NULL;
+    samconfConfigNew(&root);
+    root->key = strdup("node");
+    root->type = SAMCONF_CONFIG_VALUE_STRING;
+    root->value.string = strdup("test");
+    *state = root;
     return 0;
 }
 
-int samconfTestSamconfConfigSetBoolSuccessTeardown(UNUSED void **state) {
+int samconfTestSamconfConfigSetBoolSuccessTeardown(void **state) {
+    samconfConfig_t *root = *state;
+    samconfConfigDelete(root);
     return 0;
 }
 
-void samconfTestSamconfConfigSetBoolSuccess(UNUSED void **state) {
+void samconfTestSamconfConfigSetBoolSuccess(void **state) {
+    samconfConfig_t *root = *state;
     bool testValue[] = {true, false};
 
     samconfConfigStatusE_t status = SAMCONF_CONFIG_ERROR;
-
-    samconfConfig_t root = {
-        .parent = NULL,
-        .key = "node",
-        .type = SAMCONF_CONFIG_VALUE_STRING,
-        .value.string = "test",
-        .children = NULL,
-        .childCount = 0,
-    };
 
     TEST("samconfConfigSetBool");
     SHOULD("%s", "return SAMCONF_CONFIG_OK since given boolean value is set");
 
     for (size_t i = 0; i < ARRAY_SIZE(testValue); i++) {
-        status = samconfConfigSetBool(&root, testValue[i]);
+        status = samconfConfigSetBool(root, testValue[i]);
         assert_int_equal(status, SAMCONF_CONFIG_OK);
-        assert_int_equal(root.type, SAMCONF_CONFIG_VALUE_BOOLEAN);
-        assert_int_equal(root.value.boolean, testValue[i]);
+        assert_int_equal(root->type, SAMCONF_CONFIG_VALUE_BOOLEAN);
+        assert_int_equal(root->value.boolean, testValue[i]);
     }
 }

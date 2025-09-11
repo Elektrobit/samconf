859f138db5eacc606587b7646393b61bbe426819
Armin Butscher <armin.butscher@emlix.com>
2025-09-11 17:27:41 +0200

    Update to 0.75.11

--- cmake/project.cmake
+++ cmake/project.cmake
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-set(SAMCONF_VERSION 0.75.8)
+set(SAMCONF_VERSION 0.75.11)
 
 # Attention: Aside from the version, as many things as possible in this file
 #   should be put into functions, as this solves potential issues with commands
